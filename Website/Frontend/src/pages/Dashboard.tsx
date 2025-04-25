e6b04acc226bc060e2442ba28330cc13857089c1
Ngan Tra Bao Nguyen (3948) <ngantrabaonguyen@kms.com.vn>
2025-04-25 23:27:17 +0700

    test

--- Website/Frontend/src/pages/Dashboard.tsx
+++ Website/Frontend/src/pages/Dashboard.tsx
@@ -1,7 +1,7 @@
 export default function Dashboard() {
   return (
     <div className="p-4">
-      <h1 className="text-2xl font-bold">D Management</h1>
+      <h1 className="text-2xl font-bold">Dashboard</h1>
       {/* Các component nội dung khác tại đây */}
     </div>
   );
